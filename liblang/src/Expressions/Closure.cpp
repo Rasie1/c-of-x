3a00cbf25896ce2db46d0012ae76f80af3d4e710
Rasie1 <rasielll@gmail.com>
2017-02-07 01:30:34 +0300

    Finish reworking predicates and closures

--- liblang/src/Expressions/Closure.cpp
+++ liblang/src/Expressions/Closure.cpp
@@ -50,20 +50,6 @@ ExpPtr Closure::apply(ExpPtrArg e, Environment& env) const
     // In case of closure as predicate, it is more likely that closure's body intersects with only
     // argument subset, leaving arguments' complement in context of applied value as it was before
 
-    
-    // if (checkType<Identifier>(e))
-    // {
-    //     auto id = s_cast<Identifier>(e);
-    //     ExpPtr x = std::const_pointer_cast<Expression>(shared_from_this());
-    //     env.add(id, x, true);
-
-    //     auto expr = env.getEqual(id);
-
-    //     return id;
-    // }
-
-
-
     auto value = Identifier::unwrapIfId(e, env);
     auto newEnv = this->env;
 
@@ -75,20 +61,14 @@ ExpPtr Closure::apply(ExpPtrArg e, Environment& env) const
 
     auto evaluated = body->eval(newEnv);
 
-    if (*evaluated == *this->argument)
+    if (checkType<Identifier>(evaluated) && 
+        *evaluated == *this->argument)
     {
-        auto value = Identifier::unwrapIfId(evaluated, env);
-        env.replace(e, value);
+        auto value = Identifier::unwrapIfId(evaluated, newEnv);
+        env.addEqual(e, value);
         return e;
     }
 
-
-//    if (isExpressionQuoted(argument, env))
-//    {
-//        env = newEnv; // not correct, it should affect only bound variables
-//        return evaluated;
-//    }
-//    else
     return Identifier::unwrapIfId(evaluated, newEnv);
 }
 
