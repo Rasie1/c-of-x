35fd5796e8a5acc448f91ed49603ecef0b22b47f
Rasie1 <rasielll@gmail.com>
2017-03-14 02:15:56 +0300

    Finish d_cast

--- liblang/include/System/Cast.h
+++ liblang/include/System/Cast.h
@@ -2,12 +2,12 @@
 #include "Expressions/Operation.h"
 #include "Expressions/Intersection.h"
 
-template <class T, class... Args>
-constexpr auto d_cast(Args&&... args)
- -> decltype(std::dynamic_pointer_cast<T>(std::forward<Args>(args)...))
-{
-    return std::dynamic_pointer_cast<T>(std::forward<Args>(args)...);
-}
+// template <class T, class... Args>
+// constexpr auto d_cast(Args&&... args)
+//  -> decltype(std::dynamic_pointer_cast<T>(std::forward<Args>(args)...))
+// {
+//     return std::dynamic_pointer_cast<T>(std::forward<Args>(args)...);
+// }
 
 template <class T>
 constexpr bool typeEquals(const std::shared_ptr<const Expression>& e)
@@ -15,25 +15,45 @@ constexpr bool typeEquals(const std::shared_ptr<const Expression>& e)
     return typeid(*e) == typeid(T);
 }
 
-// template <class T, class... Args>
-// auto d_cast(Args&&... args)
-//  -> decltype(std::dynamic_pointer_cast<T>(std::forward<Args>(args)...))
-// {
-//     if (auto casted = std::dynamic_pointer_cast<T>(std::forward<Args>(args)...))
-//         return casted;
-//     if (auto op = std::dynamic_pointer_cast<Operation>(std::forward<Args>(args)...))
-//     if (typeEquals<Intersection>(op->op))
-//     {
-//         if (auto l = d_cast<T>(op->left))
-//             return l;
-//         if (auto r = d_cast<T>(op->right))
-//             return r;
-//     }
+template <class T, class F>
+auto d_cast(const std::shared_ptr<F>& e)
+ -> decltype(std::dynamic_pointer_cast<T>(e))
+{
+    if (auto casted = std::dynamic_pointer_cast<T>(e))
+        return casted;
+    if (auto op = std::dynamic_pointer_cast<Operation>(e))
+    if (typeEquals<Intersection>(op->op))
+    {
+        if (auto l = d_cast<T>(op->left))
+            return l;
+        if (auto r = d_cast<T>(op->right))
+            return r;
+    }
 
-//     // return std::dynamic_pointer_cast<T>(std::forward<Args>(args)...);
+    // return std::dynamic_pointer_cast<T>(std::forward<Args>(args)...);
 
-//     return nullptr;
-// }
+    return nullptr;
+}
+
+template <class T, class F>
+auto d_cast(const std::shared_ptr<const F>& e)
+ -> decltype(std::dynamic_pointer_cast<const T>(e))
+{
+    if (auto casted = std::dynamic_pointer_cast<const T>(e))
+        return casted;
+    if (auto op = std::dynamic_pointer_cast<const Operation>(e))
+    if (typeEquals<const Intersection>(op->op))
+    {
+        if (auto l = d_cast<T>(op->left))
+            return l;
+        if (auto r = d_cast<T>(op->right))
+            return r;
+    }
+
+    // return std::dynamic_pointer_cast<T>(std::forward<Args>(args)...);
+
+    return nullptr;
+}
 
 template <class T>
 constexpr bool checkType(const std::shared_ptr<const Expression>& e)
