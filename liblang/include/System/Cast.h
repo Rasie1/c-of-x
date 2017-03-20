f105477c84c4c813238ef8b7d1c46955f82d7529
Rasie1 <rasielll@gmail.com>
2017-03-20 11:07:41 +0300

    Return cast functions for pointers

--- liblang/include/System/Cast.h
+++ liblang/include/System/Cast.h
@@ -9,21 +9,25 @@ constexpr bool typeEquals(const std::shared_ptr<const Expression>& e)
     return typeid(*e) == typeid(T);
 }
 
+template <class T>
+constexpr bool typeEquals(const Object& e)
+{
+    return typeid(*e) == typeid(T);
+}
+
 template <class T, class F>
 auto cast_helper_visitor(const std::shared_ptr<F>& e)
  -> decltype(std::dynamic_pointer_cast<T>(e))
 {
     if (auto casted = std::dynamic_pointer_cast<T>(e))
         return casted;
-    if (auto op = std::dynamic_pointer_cast<const Operation>(e))
+    if (auto op = std::dynamic_pointer_cast<Operation>(e))
+    if (typeEquals<Intersection>(op->op))
     {
-        if (typeEquals<const Intersection>(op->op))
-        {
-            if (auto l = cast_helper_visitor<T>(op->left))
-                return l;
-            if (auto r = cast_helper_visitor<T>(op->right))
-                return r;
-        }
+        if (auto l = cast_helper_visitor<T>(op->left.expression))
+            return l;
+        if (auto r = cast_helper_visitor<T>(op->right.expression))
+            return r;
     }
 
     return nullptr;
@@ -50,9 +54,28 @@ auto d_cast(const Object& e)
     return d_cast_helper<T>(e.expression);    
 }
 
+template <class T, class F>
+auto d_cast(const std::shared_ptr<F>& e)
+ -> decltype(cast_helper_visitor<T>(e))
+{
+    return cast_helper_visitor<T>(e);    
+}
+
+template <class T, class F>
+auto d_cast(const std::shared_ptr<const F>& e)
+ -> decltype(cast_helper_visitor<const T>(e))
+{
+    return cast_helper_visitor<const T>(e);    
+}
+
 template <class T>
 constexpr bool checkType(const Object& e)
 {
-    // return typeid(*e) == typeid(T);
-    return d_cast<T>(e) != Object();
+    return d_cast<T>(e) != nullptr;
+}
+
+template <class T>
+constexpr bool checkType(const std::shared_ptr<const Expression>& e)
+{
+    return d_cast<const T>(e) != nullptr;
 }
