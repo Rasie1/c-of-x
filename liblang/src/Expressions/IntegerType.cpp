b67a903497f1fe8973fc7851b4f5a4c44bc31b84
Rasie1 <rasielll@gmail.com>
2017-05-10 10:20:14 +0300

    Work on cast-eval

--- liblang/src/Expressions/IntegerType.cpp
+++ liblang/src/Expressions/IntegerType.cpp
@@ -2,13 +2,23 @@
 #include "Expressions/Integer.h"
 #include "Expressions/Void.h"
 #include "Expressions/Identifier.h"
+#include "Expressions/ValueInSet.h"
 #include "System/Environment.h"
 #include "System/Cast.h"
 
 optional<bool> IntegerType::holds(const Object& e, Environment& env)
 {
-    auto ret = checkType<Integer>(env, e);
-    return make_optional(ret);
+    if (auto i = cast<Integer>(env, e))
+    {
+        return make_optional(true);
+    }
+    if (auto valueInSet = cast<ValueInSet>(env, e))
+    if (auto integerType = cast<IntegerType>(env, valueInSet->set))
+    {
+        return make_optional(true);
+    }
+
+    return make_optional(false);
 }
 
 std::string IntegerType::show() const
